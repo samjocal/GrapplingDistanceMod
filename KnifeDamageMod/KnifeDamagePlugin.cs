843661ce6969704db443f52e688851747d0d1816
scald <scald@THE_BRICK>
2025-04-21 00:17:57 -0500

    Add project files.

---
+++ KnifeDamageMod/KnifeDamagePlugin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace KnifeDamageMod
+{
+    [BepInPlugin(MyGuid, PluginName, VersionString)]
+    [BepInDependency("com.snmodding.nautilus")] // marks Nautilus as a dependency for this mod
+
+    public class KnifeDamagePlugin : BaseUnityPlugin
+    {
+        private const string MyGuid = "com.samjocal.knifedamagemod";
+        private const string PluginName = "Knife Damage Mod";
+        private const string VersionString = "1.0.0";
+
+        private static readonly Harmony Harmony = new Harmony(MyGuid);
+
+        public static ManualLogSource Log;
+
+        private void Awake()
+        {
+            Harmony.PatchAll();
+            Logger.LogInfo(PluginName + " " + VersionString + " loaded.");
+            Log = Logger;
+        }
+    }
+}
