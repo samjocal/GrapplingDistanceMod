97aad863c598a9d8153a63710476331ff4de54ce
samjocal <scaldwell126@gmail.com>
2025-06-06 13:41:20 -0500

    Initialized ReaperCompanionMod Environment

---
+++ ReaperCompanionMod/ReaperCompanionPlugin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using static OVRPlugin;
+
+namespace Samjocal.ReaperCompanionMod
+{
+    [BepInPlugin(MyGuid, PluginName, VersionString)]
+    [BepInDependency("com.snmodding.nautilus")]
+    public class ReaperCompanionPlugin : BaseUnityPlugin
+    {
+        private const string MyGuid = "com.samjocal.reapercompanionmod";
+        private const string PluginName = "Reaper Companion Mod";
+        private const string VersionString = "1.0.0";
+
+        private static readonly Harmony Harmony = new Harmony(MyGuid);
+
+        public static ManualLogSource Log;
+
+        private void Awake()
+        {
+            Harmony.PatchAll();
+            Logger.LogInfo(PluginName + " " + VersionString + " loaded.");
+            Log = Logger;
+        }
+    }
+}
